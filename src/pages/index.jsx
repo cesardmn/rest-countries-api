e7744a2a81bb4c2e321aa9c37951a93c786768a9
cesardmn <cesardmn@gmail.com>
2023-05-03 13:55:16 -0300

    Starting dark mode styles.

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,15 @@
 import Head from 'next/head'
-import { Inter } from 'next/font/google'
+import { Nunito } from 'next/font/google'
+
+import { useToggleTheme } from '@providers/ToggleThemeProvider'
 
 import Navbar from '@components/Navbar'
 
-const inter = Inter({ subsets: ['latin'] })
+const nunito = Nunito({ subsets: ['latin'] })
 
 export default function Home() {
+  const { toggleTheme } = useToggleTheme()
+
   return (
     <>
       <Head>
@@ -14,7 +18,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={inter.className}>
+      <main className={`${nunito.className}  ${toggleTheme}`}>
         <Navbar />
       </main>
     </>
