4c7cba3459e03e8678cef118b5ac89b080d6647c
cesardmn <cesardmn@gmail.com>
2023-05-07 22:49:47 -0300

    Adjusting CountryCard styles.

--- src/components/CountryCard/index.jsx
+++ src/components/CountryCard/index.jsx
@@ -1,11 +1,28 @@
+import styles from '@components/CountryCard/CountryCard.module.css'
+
+const { card, informations, flag, field } = styles
+
 export default function CountryCard({ country }) {
   return (
-    <li key={country.name.common}>
-      <img src={country.flags.png} alt="country flag" />
-      <h3>{country.name.common}</h3>
-      <p>population: {country.population}</p>
-      <p>region: {country.region}</p>
-      <p>capital: {country.capital}</p>
+    <li key={country.name.common} className={card}>
+      <div className={flag}>
+        <img src={country.flags.png} alt="country flag" />
+      </div>
+      <div className={informations}>
+        <h3>{country.name.common}</h3>
+        <p>
+          <span className={field}>population: </span>
+          {country.population}
+        </p>
+        <p>
+          <span className={field}>region: </span>
+          {country.region}
+        </p>
+        <p>
+          <span className={field}>capital: </span>
+          {country.capital}
+        </p>
+      </div>
     </li>
   )
 }
