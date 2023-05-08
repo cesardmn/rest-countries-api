785d465dd979354e44bd10731518ae6821afe1b2
cesardmn <cesardmn@gmail.com>
2023-05-08 03:38:38 -0300

    Click through to the border countries on the detail page.

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Integrating with the REST Countries API to pull countries data.
 - [x] Search for a country using an input field.
 - [x] Filter countries by region.
 - [ ] Click on a country to see more detailed information on a separate page.
-- [ ] Click through to the border countries on the detail page.
+- [x] Click through to the border countries on the detail page.
 - [x] Toggle the color scheme between light and dark mode (optional).
 
 [Inspired by frontendmentor.](https://www.frontendmentor.io/challenges/rest-countries-api-with-color-theme-switcher-5cacc469fec04111f7b848ca)
---
+++ src/pages/detail.jsx
@@ -0,0 +1,87 @@
+import { useRouter } from 'next/router'
+import React, { useState, useEffect } from 'react'
+
+export default function Detail() {
+  const [data, setData] = useState([])
+  const [bordersData, setBordersData] = useState({})
+  const router = useRouter()
+  const { country_name } = router.query
+  const url = country_name
+    ? `https://restcountries.com/v3.1/name/${country_name}`
+    : null
+
+  useEffect(() => {
+    async function fetchData() {
+      if (url) {
+        const response = await fetch(url)
+        const json = await response.json()
+        setData(json[0])
+      }
+    }
+    fetchData()
+  }, [url])
+
+  useEffect(() => {
+    async function fetchBordersData() {
+      if (data.borders) {
+        const bordersInfo = {}
+        for (const border of data.borders) {
+          const response = await fetch(
+            `https://restcountries.com/v3.1/alpha/${border}`
+          )
+          const json = await response.json()
+          bordersInfo[border] = json[0]
+        }
+        setBordersData(bordersInfo)
+      }
+    }
+    fetchBordersData()
+  }, [data.borders])
+
+  const borders = data.borders || []
+
+  return (
+    <div className="card">
+      {data.flags?.png && (
+        <>
+          <img src={data.flags.png} alt={data.flags.alt} />
+          <h3>{data.name.common}</h3>
+
+          <span className="field">native name: </span>
+
+          {data.name.nativeName[Object.keys(data.name.nativeName)[0]].common}
+
+          <span className="field">population: </span>
+          {data.population}
+
+          <span className="field">sub region: </span>
+          {data.subregion}
+
+          <span className="field">capital: </span>
+          {data.capital}
+
+          <span className="field">top level domain: </span>
+          {data.cca2}
+
+          <span className="field">currency: </span>
+          {data.currencies[Object.keys(data.currencies)[0]].name}
+
+          <span className="field">languages: </span>
+          {data.languages[Object.keys(data.languages)[0]]}
+
+          <p>
+            <span className="field">borders: </span>
+            {borders.map((border) => (
+              <a
+                key={border}
+                href={`./detail?&country_name=${bordersData[border]?.name?.common}`}
+              >
+                {bordersData[border]?.name?.common || 'Loading...'}
+              </a>
+            ))}
+          </p>
+        </>
+      )}
+    </div>
+  )
+}
